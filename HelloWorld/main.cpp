332c902c578d553cbb67b38e784e7b761dd56154
Felicia Redelaar <fredelaar@gmail.com>
2019-01-07 12:45:38 +0100

    GuessingGame

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -1,15 +1,41 @@
-#include <cstdlib>
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
 int main()
 {
-	char name[20];
-	cout << "Enter your name:";
-	cin >> name;
-	cout << "Hello " << name << "! \n";
+	srand(time(nullptr));
 
-	system("PAUSE");
-	return EXIT_SUCCESS;
+	int theNumber;
+	int ui;
+	bool flag = false;
+
+	theNumber = rand() % 100 + 1;
+
+	cout << theNumber;
+
+	cout << "Enter a number please :-) " << endl;
+	cin >> ui;
+
+	while (flag == false)
+	{
+		if (ui > theNumber)
+		{
+			cout << "Try again. The number is smaller." << endl;
+			cin >> ui;
+		}
+		if (ui < theNumber)
+		{
+			cout << "Try again. The number is bigger." << endl;
+			cin >> ui;
+		}
+		if (ui == theNumber)
+		{
+			cout << "Congratulations! You have won!" << endl;
+			flag = true;
+		}
+}
+	return 0;
 }
